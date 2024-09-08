175179efca8a28fa1672215733d1eec102e7a70e
Никита Касимов <kasimov.n@incasecorp.ru>
2024-09-08 16:31:58 +0300

    fafas

--- js/main.js
+++ js/main.js
@@ -8,3 +8,60 @@
         }
     };
 })(); //применяет стиль при имзменение скролла
+
+(function () {
+    const burger = document.querySelector(".burger");
+    const menu = document.querySelector(".header__nav");
+    const menuCloseItem = document.querySelector(".header__nav-close");
+    burger.addEventListener("click", () => {
+        menu.classList.add("header__nav-active");
+    });
+    menuCloseItem.addEventListener("click", () => {
+        menu.classList.remove("header__nav-active");
+    });
+})();
+
+// Функция осуществляющая scroll до якоря
+(function () {
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight = document.querySelector(".header").clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition =
+            target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.screenY;
+        let startTime = null;
+
+        const ease = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return (c / 2) * t * t + b;
+            t--;
+            return (-c / 2) * (t * (t - 2) - 1) + b;
+        };
+
+        const animation = function (currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(
+                timeElapsed,
+                startPosition,
+                targetPosition,
+                duration
+            );
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll(".js-scroll");
+        links.forEach((each) => {
+            each.addEventListener("click", function () {
+                //объект each является контекстом
+                const currentTarget = this.getAttribute("href");
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+})();
